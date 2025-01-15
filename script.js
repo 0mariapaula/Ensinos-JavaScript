2db006986a21fd6c1e321a91231de4df3c8b2379
0mariapaula <0mariapaulaferreira@gmail.com>
2025-01-15 20:31:10 -0300

    Exercicio de variaveis

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
-let idade  = 90 /* Number*/
-let nome = "Lulu"  /*String*/
-let logado = false /*Boolean*/
-let  lista = ["ovo" , "leite" , "farinha"] /*Array*/
-let  nomeCompleto = {nome: "lulu" , idade : 90}
+//let idade  = 90 /* Number*/
+//let nome = "Lulu"  /*String*/
+//let logado = false /*Boolean*/
+//let  lista = ["ovo" , "leite" , "farinha"] /*Array*/
+//let  nomeCompleto = {nome: "lulu" , idade : 90}
 
 /* Informação adc*/
-let pessoa = null; // A variável pessoa não tem valor, está intencionalmente vazia
+//let pessoa = null; // A variável pessoa não tem valor, está intencionalmente vazia
 
-let nomi;
-console.log(nomi);  // undefined, pois a variável 'nome' foi declarada, mas não recebeu valor ainda
+//let nomi;
+//console.log(nomi);  // undefined, pois a variável 'nome' foi declarada, mas não recebeu valor ainda
 
-let numeroGrande = 1234567890123456789012345678901234567890n; // BigInt
-console.log(numeroGrande);
+//let numeroGrande = 1234567890123456789012345678901234567890n; // BigInt
+//console.log(numeroGrande);
 
-let idUsuario = Symbol('id');
-let usuario = {
-  [idUsuario]: 12345 // Usando o Symbol como chave única
-};
-console.log(usuario[idUsuario]); // 12345
+//let idUsuario = Symbol('id');
+//let usuario = {
+//  [idUsuario]: 12345 // Usando o Symbol como chave única
+//};
+//console.log(usuario[idUsuario]); // 12345
 
+//console.log ("Oi eu sou a/o" , nome , " e tenho " , idade , " anos")*/
 
-
-console.log ("Oi eu sou a/o" , nome , " e tenho " , idade , " anos")
+let nomeCarro = "Ferrari"
+let bolo = 19.99
+console.log(nomeCarro , bolo)
