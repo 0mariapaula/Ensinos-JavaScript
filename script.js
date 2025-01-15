cfc678b106c870e2a1b7afe8e76b63c4a5e7b9a1
0mariapaula <0mariapaulaferreira@gmail.com>
2025-01-15 20:40:33 -0300

    Let, Const, Var

--- script.js
+++ script.js
@@ -1,26 +1,5 @@
-//let idade  = 90 /* Number*/
-//let nome = "Lulu"  /*String*/
-//let logado = false /*Boolean*/
-//let  lista = ["ovo" , "leite" , "farinha"] /*Array*/
-//let  nomeCompleto = {nome: "lulu" , idade : 90}
+let nome = "MARIA"
+nome = "PAULA"
+console.log(nome) //posso alterar o valor dela ao decorrer do desenvolvimento o VAR tambem faz igual 
 
-/* Informação adc*/
-//let pessoa = null; // A variável pessoa não tem valor, está intencionalmente vazia
-
-//let nomi;
-//console.log(nomi);  // undefined, pois a variável 'nome' foi declarada, mas não recebeu valor ainda
-
-//let numeroGrande = 1234567890123456789012345678901234567890n; // BigInt
-//console.log(numeroGrande);
-
-//let idUsuario = Symbol('id');
-//let usuario = {
-//  [idUsuario]: 12345 // Usando o Symbol como chave única
-//};
-//console.log(usuario[idUsuario]); // 12345
-
-//console.log ("Oi eu sou a/o" , nome , " e tenho " , idade , " anos")*/
-
-let nomeCarro = "Ferrari"
-let bolo = 19.99
-console.log(nomeCarro , bolo)
+// const = nao pode ser mudado ficaa com o valor ate o fim do codigo
